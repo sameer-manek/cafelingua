22b18e8854fb50728e1d1511f43687925e35ef66
Deval Koradiya <patel@devalkoradiya.in>
2018-06-08 15:26:54 +0530

    MEH

--- app/Blueprints/site/routes.py
+++ app/Blueprints/site/routes.py
@@ -508,11 +508,6 @@ def updateTest(id):
     #students appearing in the test
     #test date
 
-<<<<<<< HEAD
-    return render_template("/admin/test/update.html", test = test, batches = batches, students = students)
-
-    
-=======
     return render_template("/admin/test/update.html", test = test, batches = batches, students = students, username = fetchusername())
 
 
@@ -522,4 +517,3 @@ def fetchusername():
 
     user = Admin.query.get(session['user'])
     return str(user.first_name + ' ' + user.last_name)
->>>>>>> ccce72fb0fc41889dd78721a1a391911201cb259
