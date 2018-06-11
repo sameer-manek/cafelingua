1d3ae0278da9947178e99f13190d0b6e87187773
Deval Koradiya <patel@devalkoradiya.in>
2018-06-11 10:11:19 +0530

    Chal Chhaya chhaya

--- app/Blueprints/config.py
+++ app/Blueprints/config.py
@@ -1,8 +1,8 @@
 DEBUG = True
 #live server
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Admin_2k18@172.104.181.210/cafelingua"
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Admin_2k18@172.104.181.210/cafelingua"
 #local server
-#SQLALCHEMY_DATABASE_URI = "mysql+pymysql://sameer:poochies@127.0.0.1:3306/cafelingua"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Deval@#$1177@127.0.0.1:3306/cafelingua"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "hbe9pZqXWyLaHbjw07d3KRAbM4d3G9Af"
 
