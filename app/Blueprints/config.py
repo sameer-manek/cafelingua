41b07f5d5f01823cfad5824773e308f5e1b3d044
bruce wayne <sameer@sameer-manek.com>
2018-06-19 20:43:48 +0530

    added logs

--- app/Blueprints/config.py
+++ app/Blueprints/config.py
@@ -1,8 +1,7 @@
 DEBUG = True
 #live server
 
-# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Admin_2k18@172.104.181.210/cafelingua"
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Admin_2k18@139.59.28.151/cafelingua"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://cafelingua:__DEEPAK@172.104.181.210/cafelingua"
 #local server
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "hbe9pZqXWyLaHbjw07d3KRAbM4d3G9Af"
